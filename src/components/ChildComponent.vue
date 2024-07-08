fefa03b01483fda283b0d75a1f59d227f0adffd6
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2024-07-08 13:45:21 +0300

    props props with default

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {watchEffect} from "vue";
+
+interface Props {
+  msg?: string,
+  count?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  msg: 'Hello'
+})
+
+watchEffect(() => {
+
+})
+</script>
+
+<template>
+  <div>
+    {{msg}} {{count}}
+  </div>
+</template>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import ChildComponent from "@/components/ChildComponent.vue";
+import {ref} from "vue";
+
+const count = ref<number>(1)
+</script>
+
+<template>
+  <ChildComponent
+      :count="count"
+  />
+  <button @click="count++">Increase Count</button>
+</template>
